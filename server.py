e16b8412c7f9d20358a3599f00ac3f4b24cc1fcd
matteonistri <matteo.nistri@hotmail.com>
2016-10-05 22:56:05 +0200

    add websocket

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from flask import Flask, render_template, url_for
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, static_url_path = "/static")
-app.debug = True
+app.config['SECRET_KEY'] = 'AngusYoung'
+socketio = SocketIO(app)
 
 @app.route("/")
 def login():
@@ -12,7 +13,18 @@ def login():
 
 @app.route("/chat/<user>")
 def chat(user):
-    return render_template("chat.html", user = user)
+    return render_template("home.html", user = user)
+
+@socketio.on('mymessage')
+def message(msg):
+    print msg["author"] + ": " + msg["msg"]
+    emit('brmsg', {'text': msg['msg'], 'author': msg['author']}, broadcast = True)
+
+@socketio.on('connection')
+def connection(data):
+    print data["data"]
+
+
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    socketio.run(app, host = '0.0.0.0', port=8080)
