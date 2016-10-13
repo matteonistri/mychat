ea04f7723941dd077d3bf898f84cc1fc1adbebaf
matteonistri <matteo.nistri@hotmail.com>
2016-10-13 17:34:32 +0200

    add username in session and form

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session, redirect, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, static_url_path = "/static")
 app.config['SECRET_KEY'] = 'AngusYoung'
 socketio = SocketIO(app)
 
-@app.route("/")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if 'username' in session:
+        print "username already in session, redirecting to chat"
+        return redirect(url_for("chat"))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        session['username'] = username
+        print "username insert into session"
+        return redirect(url_for("chat"))
     return render_template("login.html")
 
-@app.route("/chat/<user>")
-def chat(user):
-    return render_template("home.html", user = user)
+@app.route("/")
+def chat():
+    if 'username' not in session:
+        print "username not in session, redirecting to login"
+        return redirect(url_for("login"))
+    print "username in session, rendering to chat"
+    return render_template("home.html", user = session['username'])
 
 @socketio.on('mymessage')
 def message(msg):
